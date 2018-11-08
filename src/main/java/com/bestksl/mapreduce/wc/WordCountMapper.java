bc16ecd8e1725d6e8742a833317b4e3fc966833f
bestksl <kslup7@gmail.com>
2018-11-08 17:31:38 -0500

    Initial commit

---
+++ src/main/java/com/bestksl/mapreduce/wc/WordCountMapper.java
@@ -0,0 +1,20 @@
+package com.bestksl.mapreduce.wc;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+
+public class WordCountMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+
+
+    @Override
+    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        String[] words = value.toString().split("");
+        for (String word : words) {
+            context.write(new Text(word), new IntWritable(1));
+        }
+    }
+}
---
+++ src/main/java/com/bestksl/mapreduce/wc/WordCountReducer.java
@@ -0,0 +1,9 @@
+package com.bestksl.mapreduce.wc;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+public class WordCountReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+
+}
