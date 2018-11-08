8e41ebec41741c668a8dd627dabeb1a3c1feb162
bestksl <kslup7@gmail.com>
2018-11-08 18:11:16 -0500

    Version 0.1

--- src/main/java/com/bestksl/mapreduce/wc/WordCountReducer.java
+++ src/main/java/com/bestksl/mapreduce/wc/WordCountReducer.java
@@ -4,6 +4,18 @@ import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
-public class WordCountReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+import java.io.IOException;
+import java.util.Iterator;
 
+public class WordCountReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+    @Override
+    protected void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+        Iterator<IntWritable> it = values.iterator();
+        int intCount = 0;
+        while (it.hasNext()) {
+            IntWritable iw = it.next();
+            intCount += iw.get();
+        }
+        context.write(key, new IntWritable(intCount));
+    }
 }
