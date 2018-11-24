97197efa013f62cb5e515028f7336e0801000d28
bestksl <kslup7@gmail.com>
2018-11-23 19:25:14 -0500

    Version 0.1

--- src/main/java/com/bestksl/mapreduce/wc/WordCountMapper.java
+++ src/main/java/com/bestksl/mapreduce/wc/WordCountMapper.java
@@ -12,7 +12,7 @@ public class WordCountMapper extends Mapper<LongWritable, Text, Text, IntWritabl
 
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-        String[] words = value.toString().split("");
+        String[] words = value.toString().split(" ");
         for (String word : words) {
             context.write(new Text(word), new IntWritable(1));
         }
